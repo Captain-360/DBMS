93a4e2216553d7b4d6a9e0ffee86ed4d807746b0
Captain-360 <captainspeaking360@gmail.com>
2026-09-16 11:19:31 +0530

    Add files via upload

---
+++ LAB-18.sql
@@ -0,0 +1,50 @@
+--From the table STUDENT perform the following queries:
+SELECT * FROM STUDENT;
+--Part – A:
+--1. Display the details of students whose SPI is greater than the average SPI.
+SELECT * FROM STUDENT
+WHERE SPI>(SELECT AVG(SPI) FROM STUDENT);
+--2. Display the names of students whose SPI is less than the average SPI.
+SELECT SNAME FROM STUDENT
+WHERE SPI<(SELECT AVG(SPI) FROM STUDENT);
+--3. Display the student details who has the highest SPI.
+SELECT * FROM STUDENT
+WHERE SPI = (SELECT MAX(SPI) FROM STUDENT);
+--4. Display the student details who has the lowest SPI.
+SELECT * FROM STUDENT
+WHERE SPI = (SELECT MIN(SPI) FROM STUDENT);
+--5. Display the students whose SPI is greater than SPI of student DHARMIK.
+SELECT * FROM STUDENT
+WHERE SPI > (SELECT SPI FROM STUDENT
+WHERE SNAME = 'DHARMIK');
+--6. Display the students whose SPI is less than SPI of student RIYA.
+SELECT * FROM STUDENT
+WHERE SPI < (SELECT SPI FROM STUDENT
+WHERE SNAME = 'RIYA')
+--7. Display the students who belong to the same branch as KRUNAL.
+
+--8. Display the students whose branch is different from HETVI.
+--9. Display the second highest SPI from RESULT table.
+--10. Display the second lowest SPI from RESULT table.
+--11. Display the names of students whose SPI is above branch-wise average SPI.
+--12. Display the branch having maximum average SPI.
+--13. Display the branch having minimum average SPI.
+--From the table STUDENT_INFO and RESULT perform the following queries:
+--Part – B:
+--14. Display the students whose SPI is greater than all students of ME branch.
+--15. Display the students whose SPI is less than any student of ME branch.
+--16. Display the student details whose SPI is not equal to any SPI of EC branch students.
+--17. Display the names of students who scored higher SPI than student of RNO 103.
+--18. Display the students whose SPI is greater than average SPI of their own branch.
+--19. Display the students whose SPI is greater than the average SPI of CE branch but greater than the
+--maximum SPI of ME branch.
+--20. Display the branch names whose average SPI is greater than the overall average SPI.
+--21. Display the students who have maximum SPI in their respective branch.
+--22. Display the students whose SPI is greater than their average SPI of their branch and greater than overall
+--average SPI.
+--Part – C:
+--23. Display the students whose SPI is greater than at least one student of every branch.
+--24. Display the students whose SPI is less than all students of CE branch.
+--25. Display the branch that contains the student with highest SPI.
+--26. Display the students whose SPI is less than the SPI of every student in CE branch and greater than every
+--student in ME branch.
